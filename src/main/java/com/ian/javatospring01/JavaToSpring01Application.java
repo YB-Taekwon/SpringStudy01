9282992f871770e0cdae8953d73ac2a743dedc17
YB-Taekwon <loundo001@gmail.com>
2025-03-20 21:44:57 +0900

    프로젝트 생성

---
+++ src/main/java/com/ian/javatospring01/JavaToSpring01Application.java
@@ -0,0 +1,13 @@
+package com.ian.javatospring01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaToSpring01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaToSpring01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ian/javatospring01/JavaToSpring01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ian.javatospring01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaToSpring01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
